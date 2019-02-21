48fabd9574b621da23b04ca49d1bcb4863f64230
BoncaldoJ <47466896+BoncaldoJ@users.noreply.github.com>
2019-02-21 01:07:52 -0500

    Create Variable_DB.py
    A new file that passes all variables and lists to the switch files

---
+++ Variable_DB.py
@@ -0,0 +1,81 @@
+
+
+# Lists
+mount_volList = []
+mount_crList = []
+mount_fsList = []
+mount_lsList = []
+mount_bashList = []
+inst_fLList = []
+inst_pMList = []
+inst_fMList = []
+inst_fPList = []
+inst_fTList = []
+inst_dateList = []
+inst_dNList = []
+inst_pNList = []
+inst_processName1List = []
+inst_processName2List = []
+inst_processName3List = []
+inst_fileLabelList = []
+inst_parentModifiedList = []
+inst_fileModifiedList = []
+inst_filePathList = []
+inst_appName_List = []
+inst_fSD_List = []
+inst_lSD_List = []
+inst_wifiIn_List = []
+inst_wifiOut_List = []
+inst_wiredIn_List = []
+inst_wiredOut_List = []
+inst_recentIName_List = []
+inst_recentIURL_List = []
+inst_SNoT_List = []
+inst_sURL_List = []
+
+# Tables
+di = '"Dock Items"'
+ih = '"InstallHistory"'
+nU = 'NetUsage'
+ri = 'RecentItems'
+sls = "'Spotlight-1-store'"
+bs = "BashSessions"
+S = 'Safari'
+
+
+# Columns
+sc = "Session_Commands"
+du = "Date_Updated"
+icd = "_kMDItemCreationDate"
+i = "Info"
+fL = '"File Label"'
+fT = '"File Type"'
+pM = '"Parent Modified"'
+fM = '"File Modified"'
+fP = '"File Path"'
+date = '"Date"'
+dN = '"DisplayName"'
+pN = '"ProcessName"'
+u = 'User'
+n = 'Name'
+fSD = '"FirstSeenDate"'
+lSD = '"LastSeenDate"'
+win = '"WifiIn"'
+wout = '"wifiOut"'
+wirIn = '"WiredIn"'
+wirOut = '"WiredOut"'
+App = 'App'
+application = 'APPLICATION'
+T = 'Type'
+url = "URL"
+dL = 'DOWNLOAD'
+NoT = 'Name_or_Title'
+
+
+string1 = "None"
+
+
+# Values
+macOS = "macOS Installer"
+softwareupdated = "softwareupdated"
+installer = "installer"
