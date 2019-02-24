dc632aa42902b4fd31e0ba5f06921350564d3096
BoncaldoJ <47466896+BoncaldoJ@users.noreply.github.com>
2019-02-24 17:56:45 -0500

    Update bashSessions.py

--- bashSessions.py
+++ bashSessions.py
@@ -1,50 +1,60 @@
 # Query Logic for Bash Sessions v0.1
-# Justin Boncaldo, Zachary Burnham (@zmbf0r3ns1cs) 2019
+# Justin Boncaldo (@boncaldoj), Zachary Burnham (@zmbf0r3ns1cs) 2019
 # ----------------------------------------------------------
+from mountedDevices import *
+from variable_db import *
 
 # The following is the parts removed from mountedDevices.py to be used here
     
-    elif counter == 5:
+    # elif counter == 5:
         # Define variables for Bash Commands
-        if counter == 5:
-            while L < volLength:
-                a = sc
-                b = bs
-                # SQLite Search Start
-                cursor.execute("SELECT {} FROM {} WHERE User = ? AND All_Commands LIKE ?".format(a, b),
-                                (userSearch, '%' + str(mount_volList[L]) + '%',))
-                bashOutput = cursor.fetchall()
-                e.clear()
-                for row in bashOutput:
-                    e.append(str(row[0]))
-                pos = 0
-                for (row) in bashOutput:
-                    string1 = str(e[pos])
-                    pos = pos + 1
-                    if counter == 5:
-                        mount_bashList.append(string1)
-                        mount_bashListLength: int = len(mount_bashList)
-                        if L == (volLength - 1):
-                            L = 0
-                            counter = counter + 1
-                        else:
-                            L = L + 1
-                    else:
-                        L = (volLength + 1)
-                
-                #counter = counter + 1
-                #f.append(str(row[0]))
-            #mount_bashList.append(str(f))
-            #L = L + 1
-        end = "yes"
+
+# x is defined for the list we're using, in the other y file
+# imports counter
+bashCounter = 0
+
+
+for row in x:  # mount_volList in mountedDevices file
+    L = 0
+    # SQLite Search Start
+    while L < p:
+
+        if bashCounter == 0:
+            print("Made it in counter 0")
+            a = sc
+            b = bs
+        cursor.execute("SELECT {} FROM {} WHERE User = ? AND All_Commands LIKE ?".format(a, b), (
+            userSearch, '%' + str(x[L]) + '%',))
+        bashOutput = cursor.fetchall()
+        e.clear()
+        for i in bashOutput:
+            e.append(str(i[0]))
+        if counter == 0:
+
+
+
+
+        pos = 0
+        for n in bashOutput:
+            string1 = str(d[pos])
+            pos = pos + 1
+            mount_bashList.append(string1)
+            mount_bashListLength: int = len(mount_bashList)
+        L = L + 1
+        bashCounter = bashCounter + 1
+
+print(bashCounter)
+print(str(mount_bashList))
+
+
+
+
+end = "yes"
 
 m = 0
 z = 0
 k = 0
 
-# Bash value check
-#if not mount_bashList:
-    #mount_bashList = str("None")
 
 # Begin writing output
 while m < (volLength):
@@ -52,10 +62,21 @@ while m < (volLength):
         mount_crList[z]) + "\n" + "\t\tVolume first seen on: " + str(
         mount_fsList[z]) + "\n" + "\t\tVolume last seen on: " + str(
         mount_lsList[z]) + "\n")
-    if not mount_bashList:
-        continue
-    else:
-        file.write("\t\t" + str(mount_bashList + "\n"))
+
+
+
+
+
+
+    # if L == (volLength - 1):
+    #     L = 0
+    #     counter = counter + 1
+    # else:
+    #     L = L + 1
+    # if not mount_bashList:
+    #     continue
+    # else:
+    #     file.write("\t\t" + str(mount_bashList + "\n"))
     m = m + 1
     z = z + 1
     # while k < mount_bashListLength:
